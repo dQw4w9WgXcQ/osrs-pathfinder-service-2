d50d3e42802dde63578395c42c73e454350ce5e6
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-11-26 02:54:11 -0800

    asdf

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.runelite.net")
+    }
+    mavenLocal()
+}
+
+dependencies {
+    implementation("dev.dqw4w9wgxcq.pathfinder:commons:1.0.0")
+    implementation("dev.dqw4w9wgxcq.pathfinder:pathfinding:1.0.0")
+    implementation("io.javalin:javalin:5.6.3")
+    implementation("io.javalin.community.ssl:ssl-plugin:5.6.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+    implementation("commons-cli:commons-cli:1.5.0")
+    //https://stackoverflow.com/questions/60074168/java-lang-illegalstateexception-no-server-alpnprocessors-wiremock/69072775#69072775
+    implementation("org.eclipse.jetty:jetty-alpn-server:11.0.17")
+    implementation("org.eclipse.jetty:jetty-alpn-java-server:11.0.17")
+
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.7")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")//set to empty to replace the default JAR
+        manifest {
+            attributes["Main-Class"] = "MainKt"
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,92 @@
+import dev.dqw4w9wgxcq.pathfinder.commons.domain.Agent
+import dev.dqw4w9wgxcq.pathfinder.commons.domain.Position
+import dev.dqw4w9wgxcq.pathfinder.pathfinding.Pathfinding
+import dev.dqw4w9wgxcq.pathfinder.pathfinding.store.GraphStore
+import dev.dqw4w9wgxcq.pathfinder.pathfinding.store.LinkStore
+import io.javalin.Javalin
+import io.javalin.community.ssl.SSLPlugin
+import io.javalin.http.bodyValidator
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.ParseException
+import java.io.File
+import java.io.FileInputStream
+import kotlin.system.exitProcess
+
+val DEFAULT_AGENT = Agent(99, null, null)
+
+fun main(args: Array<String>) {
+    val certPathOpt = Option("c", "certDir", true, "Path to dir containing fullchain.pem and privkey.pem")
+    certPathOpt.isRequired = true
+    val options = Options().addOption(certPathOpt)
+    val cmd = try {
+        DefaultParser().parse(options, args)
+    } catch (e: ParseException) {
+        println(e.message)
+        HelpFormatter().printHelp("asdf", options)
+        exitProcess(1)
+    }
+
+    val certDir = cmd.getOptionValue("c")!!
+    println("certDir: $certDir")
+
+    val javalin = Javalin.create { config ->
+        config.plugins.register(SSLPlugin {
+            it.pemFromPath(
+                "$certDir/fullchain.pem",
+                "$certDir/privkey.pem"
+            )
+        })
+
+        config.plugins.enableCors { it ->
+            it.add { it.anyHost() }
+        }
+    }
+
+    val linkFile = File("links.zip")
+    if (!linkFile.exists()) {
+        println("links.zip not found")
+        exitProcess(1)
+    }
+
+    val graphFile = File("graph.zip")
+    if (!graphFile.exists()) {
+        println("graph.zip not found")
+        exitProcess(1)
+    }
+
+    val links = LinkStore.load(FileInputStream(linkFile)).links
+    val graphStore = GraphStore.load(FileInputStream(graphFile), links)
+    val pathfinding = Pathfinding.create(graphStore)
+
+    javalin
+        .get("/") {
+            it.result("Hello World")
+        }
+        .post("/request-path") { ctx ->
+            data class Req(val start: Position, val finish: Position, val agent: Agent?)
+
+            //todo: currently deserializes incorrectly if a nested primitive field is missing (finish.y will be 0)
+            // {
+            //    "start": {
+            //        "x": 3200,
+            //        "y": 3200,
+            //        "plane": 0
+            //    },
+            //    "finish": {
+            //        "x": 3201,
+            //        "plane": 0
+            //    }
+            // }
+            val req = ctx.bodyValidator<Req>().get()
+
+            val pathfindingResult = pathfinding
+                .findPath(req.start, req.finish, req.agent ?: DEFAULT_AGENT)
+
+            ctx.header("Access-Control-Max-Age", "600")
+            ctx.json(pathfindingResult)
+        }
+        .start()//if using ssl plugin, port will be ignored and will listen on 80/443
+}
