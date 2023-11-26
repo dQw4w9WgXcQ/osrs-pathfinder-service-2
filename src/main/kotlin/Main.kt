998b423e33f55069ee8ab693b5568206c23f1d43
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-11-26 03:08:20 -0800

    asdf

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,13 +13,19 @@ import org.apache.commons.cli.Options
 import org.apache.commons.cli.ParseException
 import java.io.File
 import java.io.FileInputStream
+import java.util.Date
 import kotlin.system.exitProcess
 
 val DEFAULT_AGENT = Agent(99, null, null)
 
+/*
+note to self:
+letsencrypt requires port 80 to renew certs, but we are using port 80 already.  every 90 days i need to stop the server and rewnew
+ */
 fun main(args: Array<String>) {
+    println("================Starting server================")
+
     val certPathOpt = Option("c", "certDir", true, "Path to dir containing fullchain.pem and privkey.pem")
-    certPathOpt.isRequired = true
     val options = Options().addOption(certPathOpt)
     val cmd = try {
         DefaultParser().parse(options, args)
@@ -29,16 +35,18 @@ fun main(args: Array<String>) {
         exitProcess(1)
     }
 
-    val certDir = cmd.getOptionValue("c")!!
+    val certDir = cmd.getOptionValue("c")
     println("certDir: $certDir")
 
     val javalin = Javalin.create { config ->
-        config.plugins.register(SSLPlugin {
-            it.pemFromPath(
-                "$certDir/fullchain.pem",
-                "$certDir/privkey.pem"
-            )
-        })
+        if (certDir != null) {
+            config.plugins.register(SSLPlugin {
+                it.pemFromPath(
+                    "$certDir/fullchain.pem",
+                    "$certDir/privkey.pem"
+                )
+            })
+        }
 
         config.plugins.enableCors { it ->
             it.add { it.anyHost() }
@@ -66,6 +74,8 @@ fun main(args: Array<String>) {
             it.result("Hello World")
         }
         .post("/request-path") { ctx ->
+            println(Date().toString() + " - " + ctx.ip() + " " + ctx.userAgent() + " " + ctx.header("X-Forwarded-For") + "\n" + ctx.body())
+
             data class Req(val start: Position, val finish: Position, val agent: Agent?)
 
             //todo: currently deserializes incorrectly if a nested primitive field is missing (finish.y will be 0)
